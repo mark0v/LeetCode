7bac4f4aff3609e3b548730473c372a223aa7ccd
Александр Марков <amark0vv@gmail.com>
2022-06-19 12:12:59 +0300

    Add PlusOneFind task

---
+++ LeetCode/PlusOne.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class PlusOne
+    {
+        public int[] PlusOneFind(int[] digits)
+        {
+            int carrier = 1;
+
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                int temp = digits[i] + carrier;
+
+                digits[i] = temp % 10;
+                carrier = temp / 10;
+            }
+
+            if (carrier == 1)
+            {
+                List<int> temp = new List<int>();
+
+                temp.Add(carrier);
+
+                foreach (var n in digits)
+                    temp.Add(n);
+
+                return temp.ToArray();
+            }
+
+            return digits;
+        }
+    }
+
+    //int[] digits = { 1, 9, 1, 9 };
+    //var res = new PlusOne();
+    //var output = res.PlusOneFind(digits);
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -9,7 +9,6 @@ public class Solution
     public static void Main()
     {
 
-       
 
         //Console.WriteLine("Hello, World!");
     }
